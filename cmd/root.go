ddf96a86a5b55fed09d054747604eefa3c48dde1
Alex Gostev <nyliv@pm.me>
2025-06-22 20:44:03 +0200

    func: added log level flag and zerolog integration to root command

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,18 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"os"
+	"strings"
 
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
 
-
+var logLevel string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -30,10 +32,30 @@ to quickly create a Cobra application.`,
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	// Configure logging based on log level flag
+	configureLogging()
+
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
+}
+
+func configureLogging() {
+	// Parse log level
+	level, err := zerolog.ParseLevel(strings.ToLower(logLevel))
+	if err != nil {
+		level = zerolog.InfoLevel
+		log.Warn().Str("invalid_level", logLevel).Msg("Invalid log level, defaulting to info")
+	}
+
+	// Set global log level
+	zerolog.SetGlobalLevel(level)
+
+	// Configure pretty console output
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: "2006-01-02T15:04:05Z07:00"})
+
+	log.Info().Str("level", level.String()).Msg("Logging configured")
 }
 
 func init() {
@@ -43,9 +65,10 @@ func init() {
 
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.k8s-controller-tutorial.yaml)")
 
+	// Add log level flag
+	rootCmd.PersistentFlags().StringVar(&logLevel, "log-level", "info", "Set log level: trace, debug, info, warn, error")
+
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
